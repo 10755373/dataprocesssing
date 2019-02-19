4b7574aacacbae8beca6a0de1c50b4a87f964a83
Rinus van Grunsven <rinusvanrunsven@Rinuss-MacBook-Pro.local>
2019-02-20 00:06:06 +0100

    add eda.py

--- homework/week2/eda.py
+++ homework/week2/eda.py
@@ -2,6 +2,7 @@
 # 10755373
 
 import pandas as pd
+import math
 import sys
 import csv
 import json
@@ -49,6 +50,9 @@ def load_data(filename):
     return(details_list)
 
 def save_csv(filename, details_list):
+    """
+    Write new file in which all the dictionaries are stored
+    """
     # make new file in which you store data
     with open("editedinput.csv", "w") as csvfile:
         # specify fieldnames
@@ -62,32 +66,90 @@ def save_csv(filename, details_list):
             writer.writerow(row)
 
 def use_data(details):
+    """
+    Read CSV into DataFrame
+    """
     # Read a comma-separated values (csv) file into DataFrame.
     df = pd.read_csv(details)
     return(df)
 
 def gdp_calculator(df):
+    """
+    Calculate gdp's median, mean, mode and standard deviation
+    """
     # get all gdp's
     gdp = df.loc[:, "GDP ($ per capita) dollars"]
     # sort values
     sorted_gdp = gdp.sort_values()
+    print("After printing sorted_gdp you'll notice that 193 is an outlier"
+    "and should, therefore, be removed for the remaining of this part in order to secure decent data")
+    # gdp = gdp.drop(index="Suriname", columns="GDP ($ per capita) dollars")
     # median_gdp = round(sorted_gdp.median())
-    median_gdp = sorted_gdp.median()
+    median_gdp = gdp.median()
     # mean = sum(sorted) / len(sorted)
-    mean_gdp = sorted_gdp.mean()
-
+    mean_gdp = gdp.mean()
     # mode
+    mode_gdp = gdp.mode()
+    # standard deviation
+    stdev_gdp = gdp.std()
 
-def countries_unique(filename):
-    unique_countries = []
-    # gather all values from the column "Country"
-    countries = df.loc[:, "Country"]
-    # remove any duplicates
-    clean_countries = countries.drop_duplicates(keep='first')
-    # loop over the countries and add them to a list
-    for country in clean_countries:
-        unique_countries.append(country)
-    return(unique_countries)
+def mortality_calculator(df):
+    """
+    Calculate fice number summary (Minimum, First Quartile, Median, Third Quartile and Maximum)
+    """
+    # Get column with all data
+    mortality = df.loc[:, "Infant mortality (per 1000 births)"]
+    # Sort values
+    sorted_mortality = mortality.sort_values()
+    print("After printing sorted_mortality you'll notice that 47, 221 and 223 are outliers"
+    "and should, therefore, be removed for the remaining of this part in order to secure decent data")
+
+    # Calculate Minimum
+    min_mortality = mortality.min()
+    # Calculate First Quartile
+    # firstq_mortality = np.percentile(df.mortality, 25)
+    firstq_mortality = mortality.quantile(0.25)
+    # Calculate Median
+    median_mortality = mortality.median()
+    # Calculate Third Quartile
+    # thirdq_mortality = np.percentile(df.mortality, 75)
+    thirdq_mortality = mortality.quantile(0.75)
+    # Calculate Maximum
+    max_mortality = mortality.max()
+
+def visualize(df):
+    """
+    Visualize all the data in a histogram (for gdp) as well as a boxplot (for mortality)
+    """
+    # Make a histogram
+    plt.hist(gdp)
+    plt.xlabel('Year')
+    plt.ylabel('Average rating')
+    plt.title('GDP ($ per capita) dollars')
+    plt.plot(list_years, list_averages, linewidth=1.0)
+    plt.axis([2007, 2018, 7, 10])
+    plt.grid(True)
+    plt.show()
+
+    # Make a boxplot
+    boxplot = plt.boxplot(mortality)
+    plt.show()
+
+
+
+# def countries_unique(filename):
+#     """
+#     Get one of every country and remove Suriname (since it's an outlier)
+#     """
+#     unique_countries = []
+#     # gather all values from the column "Country"
+#     countries = df.loc[:, "Country"]
+#     # remove any duplicates
+#     clean_countries = countries.drop_duplicates(keep='first')
+#     # loop over the countries and add them to a list
+#     for country in clean_countries:
+#         unique_countries.append(country)
+#     return(unique_countries)
 
 if __name__ == "__main__":
     details_list = load_data(INPUT_CSV)
@@ -95,3 +157,4 @@ if __name__ == "__main__":
     df = use_data(EDITED_INPUT_CSV)
     # countries_unique(EDITED_INPUT_CSV)
     gdp = gdp_calculator(df)
+    mortality = mortality_calculator(df)
