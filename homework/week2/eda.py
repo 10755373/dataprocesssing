d0a1b6611955d968693870cc66601f84b653ceb4
Rinus van Grunsven <rinusvanrunsven@wcw-stud-145-109-48-111.wireless.uva.nl>
2019-02-20 16:38:28 +0100

    add eda.py

--- homework/week2/eda.py
+++ homework/week2/eda.py
@@ -28,11 +28,13 @@ def load_data(filename):
             for value in ["Country", "Region", "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)", "GDP ($ per capita) dollars"]:
                 if row[value] == "unknown":
                     population = row["Population"]
+                    populationfl = population.replace(",", ".")
                     area = row["Area (sq. mi.)"]
-                    density = int(population) / int(area)
-                    density_rounded = round(density)
+                    areafl = area.replace(",", ".")
+                    density = float(populationfl) / float(areafl)
+                    # density_rounded = float(density)
                     # details_dict[value] = density_rounded
-                    details_dict[value] = density_rounded
+                    details_dict[value] = float(density)
                 elif row[value]:
                         if type(row[value]) is str:
                             row[value] = row[value].strip()
@@ -43,6 +45,10 @@ def load_data(filename):
                             mortality = row[value].replace(",", ".")
                             mortality_replaced = float(mortality)
                             row[value] = mortality_replaced
+                        if value == "Pop. Density (per sq. mi.)":
+                            density = row[value].replace(",", ".")
+                            densityfl = float(density)
+                            row[value] = densityfl
                         details_dict[value] = row[value]
                 else:
                     details_dict[value] = 0
@@ -81,18 +87,20 @@ def gdp_calculator(df):
     gdp = df.loc[:, "GDP ($ per capita) dollars"]
     # sort values
     sorted_gdp = gdp.sort_values()
-    print("After printing sorted_gdp you'll notice that 193 is an outlier"
-    "and should, therefore, be removed for the remaining of this part in order to secure decent data")
+    print("After printing sorted_gdp, I noticed that country 195 (Suriname) was an outlier since it's GDP is $400.000. "
+    "Therefore, I've removed it for the remaining of this part in order to secure a decent data")
     # gdp = gdp.drop(index="Suriname", columns="GDP ($ per capita) dollars")
-    # median_gdp = round(sorted_gdp.median())
-    median_gdp = gdp.median()
-    # mean = sum(sorted) / len(sorted)
-    mean_gdp = gdp.mean()
-    # mode
-    mode_gdp = gdp.mode()
-    # standard deviation
-    stdev_gdp = gdp.std()
-    return(gdp)
+    cleaned_gdp = gdp.drop([193])
+    # Calculate the median
+    median_gdp = cleaned_gdp.median()
+    # Calculate the mean
+    mean_gdp = cleaned_gdp.mean()
+    # Calculate the mode
+    mode_gdp = int(cleaned_gdp.mode())
+    # Calculate the standard deviation
+    stdev_gdp = cleaned_gdp.std()
+    print("The median is {}, the mean is {}, the mode is {} and the standard deviation is {}".format(median_gdp, mean_gdp, mode_gdp, stdev_gdp))
+    return(cleaned_gdp)
 
 def mortality_calculator(df):
     """
@@ -102,68 +110,58 @@ def mortality_calculator(df):
     mortality = df.loc[:, "Infant mortality (per 1000 births)"]
     # Sort values
     sorted_mortality = mortality.sort_values()
-    print("After printing sorted_mortality you'll notice that 47, 221 and 223 are outliers"
-    "and should, therefore, be removed for the remaining of this part in order to secure decent data")
-
+    # print first message
+    print("After printing sorted_mortality I noticed that country 7 (Angola) was an outliers since it's mortality rate is 191,19. "
+    "Therefore, I removed it in the remaining of this part in order to secure a decent data set")
+    # Remove the outliers from list
+    mortality_cleaned = mortality.drop([5])
     # Calculate Minimum
-    min_mortality = mortality.min()
+    min_mortality = mortality_cleaned.min()
     # Calculate First Quartile
-    # firstq_mortality = np.percentile(df.mortality, 25)
-    firstq_mortality = mortality.quantile(0.25)
+    firstq_mortality = mortality_cleaned.quantile(0.25)
     # Calculate Median
-    median_mortality = mortality.median()
+    median_mortality = mortality_cleaned.median()
     # Calculate Third Quartile
-    # thirdq_mortality = np.percentile(df.mortality, 75)
-    thirdq_mortality = mortality.quantile(0.75)
+    thirdq_mortality = mortality_cleaned.quantile(0.75)
     # Calculate Maximum
-    max_mortality = mortality.max()
-    return(mortality)
-
-# def countries_unique(filename):
-#     """
-#     Get one of every country
-#     """
-#     unique_countries = []
-#     # gather all values from the column "Country"
-#     countries = df.loc[:, "Country"]
-#     # remove any duplicates
-#     clean_countries = countries.drop_duplicates(keep='first')
-#     # loop over the countries and add them to a list
-#     for country in clean_countries:
-#         unique_countries.append(country)
-#     return(unique_countries)
+    max_mortality = mortality_cleaned.max()
+    # print the message with all the requested info
+    print("The minimum is {}, the first quartile is {}, the median is {}, the third quartile is {} and the maximum is {}".format(min_mortality, firstq_mortality,
+    median_mortality, thirdq_mortality, max_mortality))
+    return(mortality_cleaned)
 
 def visualize(df):
     """
     Visualize all the data in a histogram (for gdp) as well as a boxplot (for mortality)
     """
     # Make a histogram
-    plt.hist(gdp)
-    plt.xlabel('Number of countries')
-    plt.ylabel('GDP')
+    width = [2]
+    plt.hist(gdp_calculator(df), width=width, bins=225)
+    plt.xlabel('GDP')
+    plt.ylabel('Number of countries')
     plt.title('GDP ($ per capita) dollars')
+    plt.grid()
     plt.show()
-
     # Make a boxplot
-    boxplot = plt.boxplot(mortality)
+    boxplot = plt.boxplot(mortality_calculator(df))
     plt.show()
 
 def make_json(details_list):
     """
     Iterate over list with all countries' details and append to a .json file
     """
+    # Create a dictionary in which you store as key the name of the country and the details from it as values
     json_dict = {}
     # Iterate over all the data
     for row in details_list:
         country_dict = {}
-        country = row["Country"]
         country_dict["Region"] = row["Region"]
         country_dict["Pop. Density (per sq. mi.)"] = row["Pop. Density (per sq. mi.)"]
         country_dict["Infant mortality (per 1000 births)"] = row["Infant mortality (per 1000 births)"]
         country_dict["GDP ($ per capita) dollars"] = row["GDP ($ per capita) dollars"]
         # Append data to a dictionary
-        json_dict[country] = country_dict
-        # Create a .json file and append dictionary to it
+        json_dict[row["Country"]] = country_dict
+        # Create a .json file and append json_dictionary to it
         with open("data.json", "w") as newfile:
             json.dump(json_dict, newfile)
 
@@ -171,7 +169,6 @@ if __name__ == "__main__":
     details_list = load_data(INPUT_CSV)
     save_csv("datainput.csv", details_list)
     df = use_data(EDITED_INPUT_CSV)
-    # countries_unique(df)
     gdp = gdp_calculator(df)
     mortality = mortality_calculator(df)
     make_json(details_list)
