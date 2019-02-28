6b9099f219307bd91bbe0baf38234f53af85c355
10755373 <45428152+10755373@users.noreply.github.com>
2019-02-28 23:34:59 +0100

    Delete knmi.js

--- homework/week3/knmi.js
+++
@@ -1,9 +0,0 @@
-var fileName = "data.json";
-var txtFile = new XMLHttpRequest();
-txtFile.onreadystatechange = function() {
-    if (txtFile.readyState === 4 && txtFile.status == 200) {
-        console.log(JSON.parse(txtFile.responseText));
-    }
-}
-txtFile.open("GET", fileName);
-txtFile.send();
