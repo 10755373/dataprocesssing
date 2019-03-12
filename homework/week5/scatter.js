d82268a54b6d37f446d17edbb34d210d3d80c392
10755373 <rinusvangrunsven@gmail.com>
2019-03-12 13:15:16 +0100

    scatter.js

--- homework/week5/scatter.js
+++ homework/week5/scatter.js
@@ -3,11 +3,15 @@
 
 // var life_expectancy = "https://stats.oecd.org/index.aspx?r=467292&erroCode=403&lastaction=login_submit#"
 // var min_wage = "https://stats.oecd.org/index.aspx?r=467292&erroCode=403&lastaction=login_submit#"
+//
+// var le = "https://stats.oecd.org/SDMX-JSON/data/RMW/AUS+BEL+CAN+CHL+COL+CZE+EST+FRA+DEU+GRC+HUN+IRL+ISR+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+POL+PRT+SVK+SVN+ESP+TUR+GBR+USA+CRI+BRA+RUS.EXR+PPP.H+A/all?startTime=2000&endTime=2017&dimensionAtObservation=allDimensions"
+// var rmw = "https://stats.oecd.org/SDMX-JSON/data/RMW/AUS+BEL+CAN+CHL+COL+CZE+EST+FRA+DEU+GRC+HUN+IRL+ISR+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+POL+PRT+SVK+SVN+ESP+TUR+GBR+USA+CRI+BRA+RUS.EXR+PPP.H+A/all?startTime=2000&endTime=2017&dimensionAtObservation=allDimensions"
+//
+// var requests = [d3.json(le), d3.json(rmw)];
 
-var le = "https://stats.oecd.org/SDMX-JSON/data/RMW/AUS+BEL+CAN+CHL+COL+CZE+EST+FRA+DEU+GRC+HUN+IRL+ISR+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+POL+PRT+SVK+SVN+ESP+TUR+GBR+USA+CRI+BRA+RUS.EXR+PPP.H+A/all?startTime=2000&endTime=2017&dimensionAtObservation=allDimensions"
-var rmw = "https://stats.oecd.org/SDMX-JSON/data/RMW/AUS+BEL+CAN+CHL+COL+CZE+EST+FRA+DEU+GRC+HUN+IRL+ISR+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+POL+PRT+SVK+SVN+ESP+TUR+GBR+USA+CRI+BRA+RUS.EXR+PPP.H+A/all?startTime=2000&endTime=2017&dimensionAtObservation=allDimensions"
-
-var requests = [d3.json(le), d3.json(rmw)];
+var gde = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/G_PPP.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
+var ppp = "https://stats.oecd.org/SDMX-JSON/data/PPPGDP/PPP.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
+var requests = [d3.json(gde), d3.json(ppp)];
 
 window.onload = function() {
   Promise.all(requests).then(function(response) {
@@ -17,92 +21,81 @@ window.onload = function() {
         console.log(refined_data)
         year = undefined
         let draw = main(refined_data, year)
-  }).catch(function(e){
-      throw(e);
-  });
-};
+        }).catch(function(e){
+          console.log(e);
+            throw(e);
+        });
+      };
 
-  // use d3 to make the plot
-  // define heigth, width, padding
-  var padding = 30;
-  var margin = {top: 40, right: 120, bottom: 20, left: 30},
-          height = 500 - margin.left - margin.right,
-          width = 800 - margin.bottom - margin.top;
+function main(refined_data, year){
+  if ((year == undefined) || (year == "")){
+    data = refined_data
+  } else {
+    data = []
+    for (let i = 0; i < refined_data.length; i++){
+      if (refined_data[i][0] == year){
+        // add data to data thingy
+        data.push(refined_data[i])
+      }
+    }
+  }};
 
-  // create a svg canvas
-  var svg = d3.select("body")
-              .append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-		 		          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // create legend block
-  var legend = svg.append("rect")
-                 .attr("class", "legend")
-                 .attr("width", margin.right)
-                 .attr("height", margin.top * 3)
-                 .attr("x", width + margin.left)
-                 .attr("y", 0);
-                 // .style("fill", "rgb(0, 255, 0)");
+function transformResponse(response){
+    // set array to add response to
+    var dataCollection = []
+    response.forEach(function(data){
+      // access data property of the response
+      let dataHere = data.dataSets[0].series;
+      console.log(dataHere)
+      // access variables in the response and save length for later
+      let series = data.structure.dimensions.series;
+      let seriesLength = series.length;
+      // console.log(series)
 
 
-  // making lists of consConf [1] and womenInScience [2]
-  var consConf = []
-  for (let i = 0; i < data.length; i++){
-    consConf.push(data[i][1])
-  }
-  var womenInScience = []
-  for (let i = 0; i < data.length; i++){
-    womenInScience.push(data[i][2])
-  }
-  var yScale = d3.scaleLinear()
-                 .domain([Math.min(...consConf) - 5, Math.max(...consConf) + 1])
-                 .range([height, padding]);
+      // set up array of variables and array of lengths
+      let varArray = [];
+      let lenArray = [];
 
-  var xScale = d3.scaleLinear()
-                 .domain([Math.min(...womenInScience) -10, Math.max(...womenInScience)])
-                 .range([padding, width]);
+      series.forEach(function(serie){
+          varArray.push(serie);
+          lenArray.push(serie.values.length);
+      });
 
-  // making the circles one at a time (for later use when different colours have to be used)
-  // colours for the circles
-  color = ['#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854','#ffd92f', '#b2182b', '#ef8a62', '#762a83', '#4d9221', '#2166ac']
+      // get the time periods in the dataset
+      let observation = data.structure.dimensions.observation[0];
+      // add time periods to the variables, but since it's not included in the
+      // 0:0:0 format it's not included in the array of lengths
+      varArray.push(observation);
 
-  // making the axyi
-     var yAxis = d3.axisLeft()
-                   .scale(yScale)
-                   .ticks(10)
+      // create array with all possible combinations of the 0:0:0 format
+      let strings = Object.keys(dataHere);
 
-     var xAxis = d3.axisBottom()
-                   .scale(xScale)
-                   .ticks(11);
+      // set up output array, an array of objects, each containing a single datapoint
+      // and the descriptors for that datapoint
+      let dataArray = [];
 
-     // add axes
- 		 canvas.append("g")
- 			.attr("class", "x axis")
- 		 	.attr("transform", "translate(0," + height + ")")
- 		 	.call(x_axis)
- 		 	.append("text")
- 		  		.attr("class", "label_x")
- 		  		.attr("x", width - margin.left)
- 		  		.attr("y", 30)
- 		  		.text("Happy Planet Index");
+      // for each string that we created
+      strings.forEach(function(string){
+          // for each observation and its index
+          observation.values.forEach(function(obs, index){
+              let data = dataHere[string].observations[index];
+              if (data != undefined){
 
- 		 canvas.append("g")
- 		 	.attr("class", "y axis")
- 		 	.call(y_axis)
- 		 	.append("text")
- 		  		.attr("class", "label_y")
- 		  		.attr("x", -margin.left)
- 		  		.attr("y", -5)
- 		  		.text("Life Expectancy");
+                  // set up temporary object
+                  let tempObj = {};
 
-// Add title above barchart
-svg.append("text")
-  .attr("x", (width / 2))
-  .attr("y", 0)
-  .attr("text-anchor", "middle")
-  .style("font-size", "14px")
-  .style('fill', 'darkOrange')
-  .text("Life expectancy and real minimum wage");
-});
+                  let tempString = string.split(":").slice(0, -1);
+                  tempString.forEach(function(s, indexi){
+                      tempObj[varArray[indexi].name] = varArray[indexi].values[s].name;
+                  });
+
+                  // every datapoint has a time and ofcourse a datapoint
+                  tempObj["time"] = obs.name;
+                  tempObj["datapoint"] = data[0];
+                  dataArray.push(tempObj);
+              }
+          });
+      });
+      dataCollection.push(dataArray)
+    })};
