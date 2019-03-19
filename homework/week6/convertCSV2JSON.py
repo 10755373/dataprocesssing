16ede230da44ccc33349831bb588337c57e1a96a
10755373 <rinusvangrunsven@gmail.com>
2019-03-19 16:02:52 +0100

    week6

--- homework/week6/convertCSV2JSON.py
+++ homework/week6/convertCSV2JSON.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 input = "DP_LIVE_17032019174948957.csv"
 output = "DP_LIVE_17032019174948957.json"
-data_list = []
 
 def read_csv(filename):
     """
@@ -20,22 +19,27 @@ def read_csv(filename):
 
     with open(input, "r") as csvfile:
         # data_dict = {}
-        reader = csv.reader(csvfile, delimiter=';')
-        for row in reader:
-            if row[0] is not "LOCATION":
-                country = row[0].strip('""')
-                year = row[5].strip('""')
-                value = row[6].strip('""')
-                data_list.append({"Country" : country, "Year" : int(year), "Value" : int(value)})
-    return(data_list)
+        reader = pd.read_csv(csvfile, delimiter=';')
+        df = reader[["LOCATION", "TIME", "Value"]]
+        df_dict = df.to_dict(orient="split")
+        with open(output, "w") as jsonfile:
+            json.dump(df_dict, jsonfile)
+        # return(df_dict)
+    #     for row in reader:
+    #         if row[0] is not "LOCATION":
+    #             country = row[0].strip('""')
+    #             year = row[5].strip('""')
+    #             value = row[6].strip('""')
+    #             data_list.append({"Country" : country, "Year" : int(year), "Value" : int(value)})
+    # return(data_list)
 
-def make_json(filename):
-    """
-    Make a JSON file
-    """
-    with open(output, "w") as jsonfile:
-        json.dump(data_list, jsonfile)
+# def make_json(filename):
+#     """
+#     Make a JSON file
+#     """
+#     with open(output, "w") as jsonfile:
+#         json.dump(df_dict, jsonfile)
 
 if __name__ == "__main__":
     read_csv(input)
-    make_json(data_list)
+    # make_json(df_dict)
