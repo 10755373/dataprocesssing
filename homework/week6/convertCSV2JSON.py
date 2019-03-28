511ae49ce08782e8bb2655653a256eade14900a4
10755373 <rinusvangrunsven@gmail.com>
2019-03-28 02:43:18 +0100

    week6

--- homework/week6/convertCSV2JSON.py
+++ homework/week6/convertCSV2JSON.py
@@ -5,18 +5,13 @@ import json
 import csv
 import pandas as pd
 
-input = "DP_LIVE_17032019174948957.csv"
-output = "DP_LIVE_17032019174948957.json"
+input = "DP_LIVE_GDP.csv"
+output = "DP_LIVE_GDP.json"
 
 def read_csv(filename):
     """
     Read CSV and append a dict to a list
     """
-    # df = pd.read_csv(filename)
-    # df = df[["LOCATION", "TIME", "VALUE"]]
-    # print(df)
-
-
     with open(input, "r") as csvfile:
         # data_dict = {}
         reader = pd.read_csv(csvfile, delimiter=';')
@@ -24,22 +19,6 @@ def read_csv(filename):
         df_dict = df.to_dict(orient="split")
         with open(output, "w") as jsonfile:
             json.dump(df_dict, jsonfile)
-        # return(df_dict)
-    #     for row in reader:
-    #         if row[0] is not "LOCATION":
-    #             country = row[0].strip('""')
-    #             year = row[5].strip('""')
-    #             value = row[6].strip('""')
-    #             data_list.append({"Country" : country, "Year" : int(year), "Value" : int(value)})
-    # return(data_list)
-
-# def make_json(filename):
-#     """
-#     Make a JSON file
-#     """
-#     with open(output, "w") as jsonfile:
-#         json.dump(df_dict, jsonfile)
 
 if __name__ == "__main__":
     read_csv(input)
-    # make_json(df_dict)
--- homework/week6/script.js
+++ homework/week6/script.js
@@ -3,17 +3,14 @@ var requests = [d3v5.json("DP_LIVE_GDP.json")];
 window.onload = function() {
   Promise.all(requests).then(function(response) {
     var req = response
-    console.log(req)
     let draw = world(req)
-
-})};
+    })
+  };
 
 function world(data) {
 
-  console.log(data)
-
+  // Use function to clean data
   var data_cleaned = transformresponse(data)
-    console.log(data_cleaned)
 
   // Set margins
   var margin = {top: 70, right: 100, bottom: 20, left: 50},
@@ -30,20 +27,6 @@ function world(data) {
   	.style('fill', 'darkOrange')
     .text("Worldmap: GDP per capita");
 
-  // var svg = d3v5.select("body")
-  //             .append("svg")
-  //             .attr("id", "map")
-  //             .attr("width", width + margin.left + margin.right)
-  //             .attr("height", height + margin.top + margin.bottom)
-  //             .append("class", "map");
-
-  // var legend = svg.append("rect")
-  //              .attr("class", "legend")
-  //              .attr("width", margin.right)
-  //              .attr("height", margin.top * 8)
-  //              .attr("x", width + margin.left)
-  //              .attr("y", 0);
-
 // Get values from 2016
 var values_2016 = []
 for (let i = 0; i < data_cleaned.length; i++){
@@ -51,7 +34,6 @@ for (let i = 0; i < data_cleaned.length; i++){
     values_2016.push(data_cleaned[i][2]);
   }
 };
-console.log(values_2016)
 
 // Make dictionary with country as key and gdp (2016) as value
 var saved_dict = {}
@@ -68,7 +50,6 @@ for (let i = 0; i < data_cleaned.length; i++){
     dict_2016[data_cleaned[i][0]] = data_cleaned[i][2]
   }
 };
-console.log(dict_2016)
 
 // Determine colorscale
 var colorScale = d3v5.scaleOrdinal()
@@ -82,10 +63,6 @@ for (var key in dict_2016) {
   country_color["fillKey"] = colorScale(dict_2016[key])
   dict_2016[key] = country_color;
 }
-
-console.log(dict_2016);
-
-console.log(saved_dict);
 
 // Make worldmap visible
 var map = new Datamap({
@@ -113,30 +90,8 @@ var map = new Datamap({
     done: function(datamap) {
       datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
         setlinegraph(geography.properties.iso);
-        // console.log(geography.properties.iso);
       })}
 
-    // done: function(datamap) {
-    //   datamap.div.selectAll('.datamaps-subunit').on('click', function (geography) {
-    //     state_name = geography.properties.name
-    // //     // var fillkey_obj = datamap.options.data[state_id] || {fillColor: '#FF1493'};
-    // //     // var fillkey = fillkey_obj.fillColor;;
-    // //     // var fillkeys = Object.keys(fills);
-    // //     // var antikey = fillkeys[Math.abs(fillkeys.indexOf(fillkey) - 1)];
-    // //     // var new_fills = {
-    // //     //   [geography.id] : {
-    // //     //     fillColor: antikey
-    // //     //   }
-    // //     // }
-    // //     // datamap.updateChoropleth(new_fills)
-    //     setlinegraph(state_name);
-      // });
-      // }
-    // setProjection: function linegraph(country, data) {
-    // var projection = d3.geo.equirectangular()}
-    // d3v5.select("body").on('click', function() {
-    //     linegraph(country, data);
-    // })
     });
 
   // Make an empty graph underneath the world map
@@ -144,18 +99,6 @@ var map = new Datamap({
 
   // Make a legend
   map.legend();
-
-  //
-  // var projection = d3v5.geoMercator()
-  //                     .scale(100)
-  //                     .translate([width / 2, height / 1.5]);
-  //
-  // var path = d3v5.geoPath().projection(projection);
-  //
-  // var xScale = d3v5.scaleLinear()
-  //           .domain([Math.min(... values_2016) - 5, Math.max(... values_2016)])
-  //           .range([20, width]);
-
 
 };
 
@@ -188,16 +131,13 @@ function setgraph(country) {
      .style("font-size", "16px")
      .text("When clicked on a country you'll see the GDP per capita from 1960 to 2016");
 
-  // var formatDate = d3v5.time.format("%y");
-  // d.year = formatDate.parse(d.year);
-  // var parseTime = d3.timeParse("%Y")
+  // Get years and parse them
   var parseTime = d3v5.timeParse("%Y");
   var years = []
   for (let i = 1970; i < 2016; i++) {
       j = parseTime(i)
       years.push(j);
       }
-      console.log(years)
 
   // Get all gdp's
   var gdps = []
@@ -208,7 +148,7 @@ function setgraph(country) {
   // Determine xScale
   var xScale = d3v5.scaleTime()
                   .range([0, width])
-                  .domain([parseTime(1970), parseTime(2016)]);
+                  .domain([Math.min(... years), Math.max(... years)]);
 
   // Determine yScale
   var yScale = d3v5.scaleLinear()
@@ -256,7 +196,7 @@ function setgraph(country) {
      .text("Average GDP per capita")
      .style("font-size", "15px");
 
-     // Get data from country throughout the year
+    // Get data from country throughout the year
    var gdp_country = []
    for (let i = 0; i < graph_data.length; i++) {
      if (graph_data[i][0] == country){
@@ -285,23 +225,17 @@ function setgraph(country) {
 )};
 
 function setlinegraph(country) {
-console.log(country)
 
   // Load data
   d3v5.json("DP_LIVE_GDP.json").then(function(response){
 
+  // Get right data and put it in a variable
   var overall_data = response.data
-  console.log(overall_data)
 
+  // Determine margins
   var margin = {top: 20, right: 80, bottom: 30, left: 200},
         width = 1300 - margin.left - margin.right,
         height = 700 - margin.top - margin.bottom;
-
-  // var svg = d3.select("body")
-  //           .append("svg")
-  //           .attr("id", "lineplot")
-  //           .attr("width", width + margin.left + margin.right)
-  //           .attr("height", height + margin.top + margin.bottom);
 
   // Get data from country
   var country_data = []
@@ -309,199 +243,162 @@ console.log(country)
     if (overall_data[i][0] == country) {
       country_data.push(overall_data[i][2]);
     }}
-console.log(country_data)
 
   // Get time and parse it in the right format
   var parseTime = d3v5.timeParse("%Y")
   var years = []
   for (let i = 0; i < overall_data.length; i++) {
-      j = parseTime(i)
+    if (overall_data[i][0] == country) {
+      j = parseTime(overall_data[i][1])
       years.push(j);
-      }
- console.log(years)
-
-  // var data_cleaned = []
-  // for (i = 0; i < data_cleaned.length; i++) {
-  //   data_cleaned.push(data_cleaned[i][2]);
-  //   }
-
-  // Make xScale
-  var xScale = d3v5.scaleTime()
-                  .range([0, width])
-                  .domain([parseTime(1970), parseTime(2016)]);
+    }}
 
   // Make yScale
   var yScale = d3v5.scaleLinear()
                 .range([height, 0])
                 .domain([Math.min(... country_data), Math.max(... country_data)]);
-  //
-  // // Make xAxis
-  // var xAxis = d3v5.axisBottom()
-  //             .ticks(10)
-  //             .scale(xScale);
-  //
-  // // Make yAxis
-  // var yAxis = d3v5.axisLeft()
-  //             .ticks(10)
-  //             .scale(yScale);
 
-  // Get data from country throughout the year
-// var parseTime = d3v5.timeParse("%Y")
-var gdp_country = []
-for (let i = 0; i < overall_data.length; i++) {
-  if (overall_data[i][0] == country){
-    gdp_country.push({"Year" : overall_data[i][1], "GDP" : overall_data[i][2]});
-  }
-}
-console.log(gdp_country)
-console.log(gdp_country["Year"])
+  // Make yAxis
+  var yAxis = d3v5.axisLeft()
+              .ticks(10)
+              .scale(yScale);
 
-var parseTime = d3v5.timeParse("%Y")
-var years_country = []
-for (let i = 0; i < gdp_country.length; i++) {
-  j = parseTime(i)
-  years_country.push(j);
-}
-// console.log(years_country)
-// console.log(years_country["Year"])
+  // Get both year and gdp from specific country and put it in a dict
+  var info_country = []
+  for (let i = 0; i < country_data.length && years.length; i++) {
+      info_country.push({"Year" : years[i], "GDP" : overall_data[i][2]});
+    }
+
+  // Make xScale
+  var xScale = d3v5.scaleTime()
+                .range([0, width])
+                // .domain([d3v5.extent(years_country, function(d) { return d })]);
+                .domain([Math.min(... years), Math.max(... years)]);
+  // var proef = d3v5.extent(years_country, function(d) { return d })
+  // console.log(proef)
+
+  // Make xAxis
+  var xAxis = d3v5.axisBottom()
+              .ticks(10)
+              .scale(xScale);
 
   // Determine points
    var country_line = d3v5.line()
      .x(function(d) {
-       console.log(d)
-       console.log(parseTime(d.Year))
-       return xScale(parseTime(d.Year))
-       ;
+       return xScale(d.Year);
      })
      .y(function(d) {
-       console.log(d)
-       return yScale(d["GDP"])
-     console.log(d["GDP"]);
+       return yScale(d["GDP"]);
    });
 
-  //  var svg_y = d3.selectAll("yAxis")
-  //      svg_y.transition()
-  //           .duration(750)
-  //           .attr("newyAxis", yScale)
-  //
-  // var svg_x = d3.selectAll("xAxis")
-  //      svg_x.transition()
-  //           .duration(750)
-  //           .attr("newyAxis", xScale)
-      //
-      // // Make yaxis
-      // svg.append("g")
-      //    .attr("class", "yAxis")
-      //    .attr("transform", "translate(" + margin.left + ",0)")
-      //    .call(yAxis)
-      //    .style("font-size", "10px");
-      //
-      // // Make xaxis
-      // svg.append("g")
-      //    .attr("class", "xAxis")
-      //    .attr("transform", "translate(" + margin.left + "," + height + ")")
-      //    .call(xAxis)
-      //    .style("font-size", "10px");
+   // Place data in the right position
+   var bisectDate = d3v5.bisector(function(d) { return d; }).left;
 
+   // Select lineplot
    var svg = d3v5.selectAll("#lineplot");
 
    // Make the changes to the line
    svg.select(".line")
       .transition()
       .duration(750)
-      .attr("d", country_line(gdp_country));
+      .attr("d", country_line(info_country));
 
-  // Make sure data fits
-  var bisectDate = d3.bisector(function(d) { return d; }).left;
+  // Create class for mouseover event
+  var mouseG = svg.append("g")
+      .attr("class", "mouse-over-effects");
 
-  // var mouseG = svg.append("g")
-  //     .attr("class", "mouse-over-effects");
-  //
-  //   mouseG.append("path") // this is the black vertical line to follow mouse
-  //     .attr("class", "mouse-line")
-  //     .style("stroke", "black")
-  //     .style("stroke-width", "1px")
-  //     .style("opacity", "0");
-  //
-  //   var lines = document.getElementsByClassName('line');
-  //
-  //   var mousePerLine = mouseG.selectAll('.mouse-per-line')
-  //     .data(gdp_country)
-  //     .enter()
-  //     .append("g")
-  //     .attr("class", "mouse-per-line");
-  //
-  //   mousePerLine.append("circle")
-  //     .attr("r", 7)
-  //     .style("stroke", black)
-  //     .style("fill", "none")
-  //     .style("stroke-width", "1px")
-  //     .style("opacity", "0");
-  //
-  //   mousePerLine.append("text")
-  //     .attr("transform", "translate(10,3)");
-  //
-  //   mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
-  //     .attr('width', width) // can't catch mouse events on a g element
-  //     .attr('height', height)
-  //     .attr('fill', 'none')
-  //     .attr('pointer-events', 'all')
-  //     .on('mouseout', function() { // on mouse out hide line, circles and text
-  //       d3.select(".mouse-line")
-  //         .style("opacity", "0");
-  //       d3.selectAll(".mouse-per-line circle")
-  //         .style("opacity", "0");
-  //       d3.selectAll(".mouse-per-line text")
-  //         .style("opacity", "0");
-  //     })
-  //     .on('mouseover', function() { // on mouse in show line, circles and text
-  //       d3.select(".mouse-line")
-  //         .style("opacity", "1");
-  //       d3.selectAll(".mouse-per-line circle")
-  //         .style("opacity", "1");
-  //       d3.selectAll(".mouse-per-line text")
-  //         .style("opacity", "1");
-  //     })
-  //     .on('mousemove', function() { // mouse moving over canvas
-  //       var mouse = d3.mouse(this);
-  //       d3.select(".mouse-line")
-  //         .attr("d", function() {
-  //           var d = "M" + mouse[0] + "," + height;
-  //           d += " " + mouse[0] + "," + 0;
-  //           return d;
-  //         });
-  //
-  //       d3.selectAll(".mouse-per-line")
-  //         .attr("transform", function(d, i) {
-  //           console.log(width / mouse[0])
-  //           var xDate = x.invert(mouse[0]),
-  //               bisect = d3.bisector(function(d) { return d.date; }).right;
-  //               idx = bisect(d.values, xDate);
-  //
-  //           var beginning = 0,
-  //               end = lines[i].getTotalLength(),
-  //               target = null;
-  //
-  //           while (true){
-  //             target = Math.floor((beginning + end) / 2);
-  //             pos = lines[i].getPointAtLength(target);
-  //             if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-  //                 break;
-  //             }
-  //             if (pos.x > mouse[0])      end = target;
-  //             else if (pos.x < mouse[0]) beginning = target;
-  //             else break; //position found
-  //           }
-  //
-  //           d3.select(this).select('text')
-  //             .text(y.invert(pos.y).toFixed(2));
-  //
-  //           return "translate(" + mouse[0] + "," + pos.y +")";
-  //         });
-  //     });
-  });
-  }
+    // Make path
+    mouseG.append("path")
+      .attr("class", "mouse-line")
+      .style("stroke", "black")
+      .style("stroke-width", "1px")
+      .style("opacity", "0");
 
+    // Get lines' id
+    var lines = document.getElementsByClassName('line');
+
+    // Append data to circle
+    var mousePerLine = mouseG.selectAll('.mouse-per-line')
+      .data(info_country)
+      .enter()
+      .append("g")
+      .attr("class", "mouse-per-line");
+
+    // Append circle to mouseover line for the data display
+    mousePerLine.append("circle")
+      .attr("r", 7)
+      .style("stroke", '#000000')
+      .style("fill", "none")
+      .style("stroke-width", "1px")
+      .style("opacity", "0");
+
+    // Append text to circle
+    mousePerLine.append("text")
+      .attr("transform", "translate(10,3)");
+
+    // Make data circle
+    mouseG.append('svg:rect')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('fill', 'none')
+      .attr('pointer-events', 'all')
+      .on('mouseout', function() {
+        d3v5.select(".mouse-line")
+          .style("opacity", "0");
+        d3v5.selectAll(".mouse-per-line circle")
+          .style("opacity", "0");
+        d3v5.selectAll(".mouse-per-line text")
+          .style("opacity", "0");
+      })
+      .on('mouseover', function() {
+        d3v5.select(".mouse-line")
+          .style("opacity", "1");
+        d3v5.selectAll(".mouse-per-line circle")
+          .style("opacity", "1");
+        d3v5.selectAll(".mouse-per-line text")
+          .style("opacity", "1");
+      })
+      .on('mousemove', function() {
+        var mouse = d3v5.mouse(this);
+        d3v5.select(".mouse-line")
+          .attr("d", function() {
+            var d = "M" + mouse[0] + "," + height;
+            d += " " + mouse[0] + "," + 0;
+            return d;
+          });
+
+        // Determine data for data circles with a function
+        d3v5.selectAll(".mouse-per-line")
+          .attr("transform", function(d, i) {
+            var lines = document.getElementsByClassName('line')
+            var xDate = xScale.invert(mouse[0]),
+                bisect = d3v5.bisector(function(d) { return d.Year; }).left;
+                idx = bisect(d.GDP, xDate);
+
+            var beginning = 0,
+                end = lines[i].getTotalLength(),
+                target = null;
+
+            while (true){
+              target = Math.floor((beginning + end) / 2);
+              pos = lines[i].getPointAtLength(target);
+              if ((target === end || target === beginning) && pos.xScale !== mouse[0]) {
+                  break;
+              }
+              if (pos.xScale > mouse[0])      end = target;
+              else if (pos.xScale < mouse[0]) beginning = target;
+              else break;
+            }
+
+            d3v5.select(this).select('text')
+              .text(yScale.invert(pos.yScale).toFixed(2));
+
+            return "translate(" + mouse[0] + "," + pos.yScale +")";
+          });
+      });
+})};
+
+// Function to retrieve the right data
 function transformresponse(data) {
     var data_list = []
     for (let i = 0; i < data[0].data.length; i++){
